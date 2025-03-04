a5676eebb00a4212a8ec4731f14f7806ffc30f24
aditya-adiraju <adiraju@student.ubc.ca>
2025-03-04 08:43:20 -0800

    #3 add primary key relationships on tables and populate initial data

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 import sqlite3
 import csv
-import os.path
 from logger import info, debug, error
 
 
@@ -23,13 +22,6 @@ def ingest_csv(filename: str) -> list[tuple[str]]:
         data = [tuple(row) for row in list(reader)[1:]]
         return data
 
-def _table_exists(con: sqlite3.Connection, table: str) -> bool:
-    cur = con.cursor()
-    debug(f"Checking if {table} exists...")
-    res = cur.execute("SELECT 1 FROM sqlite_master WHERE name=?", (table,))
-    exists = res.fetchone() is not None
-    return exists
-
 def db_setup(con: sqlite3.Connection):
     """Sets up SQLite Database tables (if they don't exist already).
 
@@ -37,16 +29,59 @@ def db_setup(con: sqlite3.Connection):
         con: sqlite Database connection
     """
     cur = con.cursor()
-    if not _table_exists(con, 'discord_id_name'):
-        cur.execute("CREATE TABLE discord_id_name(discord_id, player_name, group_name)")
-        debug(f'Created table discord_id_name successfully.')
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS discord_id_name (
+                    discord_id TEXT PRIMARY KEY, 
+                    player_name TEXT NOT NULL, 
+                    group_name TEXT NOT NULL
+                )
+                """)
+    debug(f'Created table discord_id_name successfully.')
 
-    if not _table_exists(con, 'target_assignments'):
-        cur.execute("CREATE TABLE target_assignments(player_discord_id, target_discord_id, secret_word)")
-        debug(f'Created table target_assignments successfully.')
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS target_assignments (
+                    player_discord_id TEXT PRIMARY KEY, 
+                    target_discord_id UNIQUE NOT NULL, 
+                    secret_word TEXT NOT NULL
+                )
+                """)
+    debug(f'Created table target_assignments successfully.')
 
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS kill_log (
+                    player_discord_id TEXT NOT NULL, 
+                    target_discord_id TEXT PRIMARY KEY,
+                    secret_word TEXT NOT NULL,
+                    TIMESTAMP TEXT NOT NULL
+                )
+                """)
+    debug(f'Created table kill_log successfully.')
 
+def add_initial_data(con: sqlite3.Connection, csv_source_filename: str):
+    """Populates the database with initial data from given CSV file
 
+    Args:
+        con: sqlite Database connection
+        csv_source_filename: filepath to CSV data
+
+    Raises:
+        OSError: if file does not exist.
+     
+    Table headers: Discord ID, Name, Group, Target_discord_id, Secret Word 
+    """
+
+    table_data = ingest_csv(csv_source_filename)
+    discord_id_name_data = [(r[0], r[1], r[2]) for r in table_data]
+    target_assignments_data = [(r[0], r[3], r[4]) for r in table_data]
+    cur = con.cursor()
+
+    cur.executemany("INSERT OR REPLACE INTO discord_id_name VALUES(?, ?, ?)", (discord_id_name_data))
+    con.commit()
+
+    cur.executemany("INSERT OR REPLACE INTO target_assignments VALUES(?, ?, ?)", (target_assignments_data))
+    con.commit()
+
+    
 con = sqlite3.connect(DATABASE_PATH)
 info(f"Connected to database: {DATABASE_PATH} successfully")
 db_setup(con)
