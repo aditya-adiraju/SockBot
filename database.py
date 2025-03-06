3a45eb553850db0f6051a24a9e3b1b534a990beb
aditya-adiraju <adiraju@student.ubc.ca>
2025-03-06 08:36:03 -0800

    GH-5 Added database functions to retrieve player and target information

--- database.py
+++ database.py
@@ -83,7 +83,87 @@ def add_initial_data(con: sqlite3.Connection, csv_source_filename: str):
     cur.executemany("INSERT OR REPLACE INTO target_assignments (player_discord_id, target_discord_id) VALUES(?, ?)", (target_assignments_data))
     con.commit()
 
+def get_player_target(con: sqlite3.Connection, player_discord_id: str) -> str | None:
+    """Retrieves a given player's target (discord id)
+
+    Args:
+        con: sqlite Database connection
+        player_discord_id: discord_id of player
+
+    Returns:
+        the target's discord ID
+        or 
+        None if a player does not have a target
+    """
+    cur = con.cursor()
+    print(player_discord_id)
+    res = cur.execute("""
+    SELECT target_discord_id FROM target_assignments
+    WHERE player_discord_id = ?
+    """, (player_discord_id,))
+    row = res.fetchone()
+    if row is None:
+        error(f"No target associated with {player_discord_id}")
+        return None
+    target_discord_id = row[0]
+    return target_discord_id
+
+def get_player_info(con: sqlite3.Connection, discord_id: str) -> tuple[str, str, str, str] | None:
+    """Retrieves a given player's information
+
+    Args:
+        con: sqlite Database connection
+        discord_id: The Player's discord ID
+
+    Returns:
+        a player's name, group, secret_word
+        or 
+        None if a player does not exist
+    """
+    cur = con.cursor()
+    res = cur.execute("""
+    SELECT player_name, group_name, secret_word FROM player_info
+    WHERE discord_id = ?
+    """, (discord_id,))
+    if (row := res.fetchone()) is None:
+        error(f"No player associated with {discord_id}")
+        return None
+    player_name, group_name, secret_word = row[0], row[1], row[2]
+
+    return (player_name, group_name, secret_word)
+
+def get_target_info(con: sqlite3.Connection, player_discord_id: str) -> tuple[str, str, str, str] | None:
+    """Retrieves a given player's information
+
+    Args:
+        con: sqlite Database connection
+        player_discord_id: The Player's discord ID
+
+    Returns:
+        target's discord_id, the target's name, group, and secret_word
+        or 
+        None if a player does not have a target
+
+    Note: 
+        This is a convenience function that leverages use of one query instead of two
+        to retrieve target information.
+    """
+    cur = con.cursor()
+    res = cur.execute("""
+    SELECT target_assignments.target_discord_id, player_info.player_name, player_info.group_name, player_info.secret_word
+    FROM target_assignments
+    INNER JOIN player_info ON player_info.discord_id = target_assignments.target_discord_id
+    WHERE target_assignments.player_discord_id = ?
+    """, (player_discord_id,))
+    if (row := res.fetchone()) is None:
+        error(f"No target associated with {player_discord_id}")
+        return None
+    target_discord_id, player_name, group_name, secret_word = row[0], row[1], row[2], row[3]
+
+    return (target_discord_id, player_name, group_name, secret_word)
     
+
 con = sqlite3.connect(DATABASE_PATH)
 info(f"Connected to database: {DATABASE_PATH} successfully")
 db_setup(con)
+print(get_target_info(con, '1234567890'))
